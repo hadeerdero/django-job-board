dc529311768196dc4857be40f20273c21378b7d9
hadeer raouf <hadeerraouf4@gmial.com>
2021-01-19 19:46:39 +0200

    add signup & profile page to website

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import SignupForm
+from .models import Profile
+
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/accounts/profile/')
+    else:
+        form = SignupForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'registration/signup.html', context)
+
+
+def profile(request):
+    profile = Profile.objects.get(user=request.user)
+
+    context = {
+        'profile': profile
+    }
+    return render(request, 'accounts/profile.html', context)
+
+
+def profile_edit(request):
+    return render(request, 'accounts/profile_edit', {})
